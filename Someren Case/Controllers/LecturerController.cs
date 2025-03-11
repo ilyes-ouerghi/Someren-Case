ed42e25b206ed65edbd599926a538cf8423121e8
AvorniceseiAndrei <729365@student.inholland.nl>
2025-03-11 13:28:50 +0100

    Lecturer view

---
+++ Someren Case/Controllers/LecturerController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SomerenApp.Controllers
+{
+    public class LecturerController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+        public LecturerController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // Action to display all lecturers
+        public IActionResult Index()
+        {
+            var lecturers = _context.Lecturers.ToList();  // Retrieve all lecturers from the database
+            return View(lecturers);  // Return the list of lecturers to the view
+        }
+    }
+}
---
+++ Someren Case/Models/Lecturer.cs
@@ -0,0 +1,23 @@
+﻿namespace Someren_Case.Models
+{
+    public class Lecturer
+    {
+        public int LecturerID { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string PhoneNumber { get; set; }
+        public DateTime DateOfBirth { get; set; }
+
+        // Add a calculated property for Age
+        public int Age
+        {
+            get
+            {
+                var age = DateTime.Now.Year - DateOfBirth.Year;
+                if (DateTime.Now.DayOfYear < DateOfBirth.DayOfYear)
+                    age--;
+                return age;
+            }
+        }
+    }
+}
