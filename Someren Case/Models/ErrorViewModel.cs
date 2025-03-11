8c7d852f8b27d89065d01216ec6aab0c186e14a1
ilyes <729096@student.inholland.nl>
2025-03-11 10:48:13 +0100

    Add project files.

---
+++ Someren Case/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Someren_Case.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
