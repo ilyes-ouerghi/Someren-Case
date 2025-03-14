15ab1878d364faf6200fca5e06f5e0e776532147
Kian <kiankian702@gmail.com>
2025-03-14 13:40:41 +0100

    Student,assignment3

--- Someren Case/Controllers/StudentsController.cs
+++ Someren Case/Controllers/StudentsController.cs
@@ -2,6 +2,8 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Someren_Case.Data;
 using Someren_Case.Models;
+using System.Linq;
+using System.Threading.Tasks;
 
 namespace Someren_Case.Controllers
 {
@@ -14,11 +16,117 @@ namespace Someren_Case.Controllers
             _context = context;
         }
 
-        // GET: Students
+        // GET: Students - Display all students
         public async Task<IActionResult> Index()
         {
             var students = await _context.Student.ToListAsync();
             return View(students);
+        }
+
+        // GET: Students/Create - Show form to create a new student
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Students/Create - Add a new student to the database
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("StudentId,FirstName,LastName,StudentNumber,PhoneNumber,Class")] Student student)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(student);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(student);
+        }
+
+        // GET: Students/Edit/{id} - Show form to edit a student
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var student = await _context.Student.FindAsync(id);
+            if (student == null)
+            {
+                return NotFound();
+            }
+            return View(student);
+        }
+
+        // POST: Students/Edit/{id} - Save changes to an edited student
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("StudentId,FirstName,LastName,StudentNumber,PhoneNumber,Class")] Student student)
+        {
+            if (id != student.StudentId)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(student);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!StudentExists(student.StudentId))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(student);
+        }
+
+        // GET: Students/Delete/{id} - Show confirmation page to delete a student
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var student = await _context.Student
+                .FirstOrDefaultAsync(m => m.StudentId == id);
+            if (student == null)
+            {
+                return NotFound();
+            }
+
+            return View(student);
+        }
+
+        // POST: Students/Delete/{id} - Remove student from database
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var student = await _context.Student.FindAsync(id);
+            if (student != null)
+            {
+                _context.Student.Remove(student);
+                await _context.SaveChangesAsync();
+            }
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool StudentExists(int id)
+        {
+            return _context.Student.Any(e => e.StudentId == id);
         }
     }
 }
