d9e6024bc89348f5be9f189025e7cf3e7dfcdcca
Kian <kiankian702@gmail.com>
2025-03-17 10:14:20 +0100

    Student

--- Someren Case/Controllers/StudentsController.cs
+++ Someren Case/Controllers/StudentsController.cs
@@ -1,132 +1,197 @@
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.EntityFrameworkCore;
-using Someren_Case.Data;
+using System.Data.SqlClient;
+using System.Data;
 using Someren_Case.Models;
-using System.Linq;
+using System.Collections.Generic;
 using System.Threading.Tasks;
+using Microsoft.Extensions.Configuration;
 
 namespace Someren_Case.Controllers
 {
     public class StudentsController : Controller
     {
-        private readonly ApplicationDbContext _context;
+        private readonly string _connectionString;
 
-        public StudentsController(ApplicationDbContext context)
+        public StudentsController(IConfiguration configuration)
         {
-            _context = context;
+            _connectionString = configuration.GetConnectionString("DefaultConnection");
         }
 
-        // GET: Students - Display all students
+        // GET: Students
         public async Task<IActionResult> Index()
         {
-            var students = await _context.Student.ToListAsync();
+            List<Student> students = new List<Student>();
+            using (SqlConnection connection = new SqlConnection(_connectionString))
+            {
+                string query = "SELECT * FROM Student";
+                SqlCommand command = new SqlCommand(query, connection);
+                connection.Open();
+                SqlDataReader reader = await command.ExecuteReaderAsync();
+                while (reader.Read())
+                {
+                    students.Add(new Student
+                    {
+                        StudentId = (int)reader["StudentId"],
+                        FirstName = reader["FirstName"].ToString(),
+                        LastName = reader["LastName"].ToString(),
+                        StudentNumber = reader["StudentNumber"].ToString(),
+                        PhoneNumber = reader["PhoneNumber"].ToString(),
+                        Class = reader["Class"].ToString()
+                    });
+                }
+            }
+
             return View(students);
         }
 
-        // GET: Students/Create - Show form to create a new student
+        // GET: Students/Create
         public IActionResult Create()
         {
             return View();
         }
 
-        // POST: Students/Create - Add a new student to the database
+        // POST: Students/Create
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Create([Bind("StudentId,FirstName,LastName,StudentNumber,PhoneNumber,Class")] Student student)
+        public async Task<IActionResult> Create(Student student)
         {
             if (ModelState.IsValid)
             {
-                _context.Add(student);
-                await _context.SaveChangesAsync();
+                using (SqlConnection connection = new SqlConnection(_connectionString))
+                {
+                    string query =
+                        "INSERT INTO Student (FirstName, LastName, StudentNumber, PhoneNumber, Class) VALUES (@FirstName, @LastName, @StudentNumber, @PhoneNumber, @Class)";
+                    SqlCommand command = new SqlCommand(query, connection);
+                    command.Parameters.AddWithValue("@FirstName", student.FirstName);
+                    command.Parameters.AddWithValue("@LastName", student.LastName);
+                    command.Parameters.AddWithValue("@StudentNumber", student.StudentNumber);
+                    command.Parameters.AddWithValue("@PhoneNumber", student.PhoneNumber);
+                    command.Parameters.AddWithValue("@Class", student.Class);
+                    connection.Open();
+                    await command.ExecuteNonQueryAsync();
+                }
+
                 return RedirectToAction(nameof(Index));
             }
+
             return View(student);
         }
 
-        // GET: Students/Edit/{id} - Show form to edit a student
-        public async Task<IActionResult> Edit(int? id)
+        // GET: Students/Edit/{id}
+        public async Task<IActionResult> Edit(int id)
         {
-            if (id == null)
+            Student student = null;
+            using (SqlConnection connection = new SqlConnection(_connectionString))
             {
-                return NotFound();
+                string query = "SELECT * FROM Student WHERE StudentId = @StudentId";
+                SqlCommand command = new SqlCommand(query, connection);
+                command.Parameters.AddWithValue("@StudentId", id);
+                connection.Open();
+                SqlDataReader reader = await command.ExecuteReaderAsync();
+                if (reader.Read())
+                {
+                    student = new Student
+                    {
+                        StudentId = (int)reader["StudentId"],
+                        FirstName = reader["FirstName"].ToString(),
+                        LastName = reader["LastName"].ToString(),
+                        StudentNumber = reader["StudentNumber"].ToString(),
+                        PhoneNumber = reader["PhoneNumber"].ToString(),
+                        Class = reader["Class"].ToString()
+                    };
+                }
             }
 
-            var student = await _context.Student.FindAsync(id);
+            if (student == null) return NotFound();
+            return View(student);
+        }
+
+        // POST: Students/Edit/{id}
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, Student student)
+        {
+            if (id != student.StudentId) return NotFound();
+            if (ModelState.IsValid)
+            {
+                using (SqlConnection connection = new SqlConnection(_connectionString))
+                {
+                    string query =
+                        "UPDATE Student SET FirstName = @FirstName, LastName = @LastName, StudentNumber = @StudentNumber, PhoneNumber = @PhoneNumber, Class = @Class WHERE StudentId = @StudentId";
+                    SqlCommand command = new SqlCommand(query, connection);
+                    command.Parameters.AddWithValue("@FirstName", student.FirstName);
+                    command.Parameters.AddWithValue("@LastName", student.LastName);
+                    command.Parameters.AddWithValue("@StudentNumber", student.StudentNumber);
+                    command.Parameters.AddWithValue("@PhoneNumber", student.PhoneNumber);
+                    command.Parameters.AddWithValue("@Class", student.Class);
+                    command.Parameters.AddWithValue("@StudentId", student.StudentId);
+                    connection.Open();
+                    await command.ExecuteNonQueryAsync();
+                }
+
+                return RedirectToAction(nameof(Index));
+            }
+
+            return View(student);
+        }
+
+        // GET: Students/Delete/{id}
+        public async Task<IActionResult> Delete(int id)
+        {
+            Student student = null;
+
+            // Establishing the connection
+            using (SqlConnection connection = new SqlConnection(_connectionString))
+            {
+                string query = "SELECT * FROM Student WHERE StudentId = @StudentId";
+                SqlCommand command = new SqlCommand(query, connection);
+                command.Parameters.AddWithValue("@StudentId", id); // Setting the parameter for StudentId
+                connection.Open();
+
+                // Executing the query and reading the data
+                SqlDataReader reader = await command.ExecuteReaderAsync();
+                if (reader.Read())
+                {
+                    student = new Student
+                    {
+                        StudentId = (int)reader["StudentId"],
+                        FirstName = reader["FirstName"].ToString(),
+                        LastName = reader["LastName"].ToString(),
+                        StudentNumber = reader["StudentNumber"].ToString(),
+                        PhoneNumber = reader["PhoneNumber"].ToString(),
+                        Class = reader["Class"].ToString()
+                    };
+                }
+            }
+
+            // If no student is found, return NotFound.
             if (student == null)
             {
                 return NotFound();
             }
+
+            // Return the student data for deletion confirmation
             return View(student);
         }
 
-        // POST: Students/Edit/{id} - Save changes to an edited student
+    
+
+    // POST: Students/Delete/{id}
         [HttpPost]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Edit(int id, [Bind("StudentId,FirstName,LastName,StudentNumber,PhoneNumber,Class")] Student student)
-        {
-            if (id != student.StudentId)
-            {
-                return NotFound();
-            }
-
-            if (ModelState.IsValid)
-            {
-                try
-                {
-                    _context.Update(student);
-                    await _context.SaveChangesAsync();
-                }
-                catch (DbUpdateConcurrencyException)
-                {
-                    if (!StudentExists(student.StudentId))
-                    {
-                        return NotFound();
-                    }
-                    else
-                    {
-                        throw;
-                    }
-                }
-                return RedirectToAction(nameof(Index));
-            }
-            return View(student);
-        }
-
-        // GET: Students/Delete/{id} - Show confirmation page to delete a student
-        public async Task<IActionResult> Delete(int? id)
-        {
-            if (id == null)
-            {
-                return NotFound();
-            }
-
-            var student = await _context.Student
-                .FirstOrDefaultAsync(m => m.StudentId == id);
-            if (student == null)
-            {
-                return NotFound();
-            }
-
-            return View(student);
-        }
-
-        // POST: Students/Delete/{id} - Remove student from database
-        [HttpPost, ActionName("Delete")]
         [ValidateAntiForgeryToken]
         public async Task<IActionResult> DeleteConfirmed(int id)
         {
-            var student = await _context.Student.FindAsync(id);
-            if (student != null)
+            using (SqlConnection connection = new SqlConnection(_connectionString))
             {
-                _context.Student.Remove(student);
-                await _context.SaveChangesAsync();
+                string query = "DELETE FROM Student WHERE StudentId = @StudentId";
+                SqlCommand command = new SqlCommand(query, connection);
+                command.Parameters.AddWithValue("@StudentId", id);
+                connection.Open();
+                await command.ExecuteNonQueryAsync();
             }
+
             return RedirectToAction(nameof(Index));
         }
 
-        private bool StudentExists(int id)
-        {
-            return _context.Student.Any(e => e.StudentId == id);
-        }
     }
 }
